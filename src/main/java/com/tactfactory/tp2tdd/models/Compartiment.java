d075cc68e0605b2624c46d163cced9c38f6cdf3a
Antoine Cronier <antoine.cronier@tactfactory.com>
2021-02-23 12:49:22 +0100

    TU06-2

--- src/main/java/com/tactfactory/tp2tdd/models/Compartiment.java
+++ src/main/java/com/tactfactory/tp2tdd/models/Compartiment.java
@@ -2,4 +2,13 @@ package com.tactfactory.tp2tdd.models;
 
 public class Compartiment {
 
+  private String type;
+
+  public String getType() {
+    return this.type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
 }
