723aab46f4f89ef45e53b569e10f8250f1b3331a
Antoine Cronier <antoine.cronier@tactfactory.com>
2021-02-23 09:17:57 +0100

    TU01-2

---
+++ src/main/java/com/tactfactory/tp2tdd/models/Fusee.java
@@ -0,0 +1,15 @@
+package com.tactfactory.tp2tdd.models;
+
+public class Fusee {
+
+  private String name;
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+
+    return this.name;
+  }
+}
