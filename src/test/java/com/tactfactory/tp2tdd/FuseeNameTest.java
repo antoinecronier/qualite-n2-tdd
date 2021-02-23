0591e4a13a97091332cda1569c7ba005ce11feb4
Antoine Cronier <antoine.cronier@tactfactory.com>
2021-02-23 09:30:46 +0100

    TU01-1-fix-2

--- src/test/java/com/tactfactory/tp2tdd/FuseeNameTest.java
+++ src/test/java/com/tactfactory/tp2tdd/FuseeNameTest.java
@@ -1,14 +1,11 @@
 package com.tactfactory.tp2tdd;
 
-import java.util.Random;
-import org.junit.jupiter.api.Test;
-
 import com.tactfactory.tp2tdd.models.Fusee;
 import com.tactfactory.tp2tdd.utils.GenericNameTest;
+import com.tactfactory.tp2tdd.utils.StringUtils;
+import org.junit.jupiter.api.Test;
 
 public class FuseeNameTest {
-
-  private Random rand = new Random();
 
   /**
    * testNameStartWithMajOk test function.
@@ -16,24 +13,7 @@ public class FuseeNameTest {
   @Test
   public void testNameStartWithMajOk() {
     Fusee fusee = new Fusee();
-    fusee.setName(Character.toString((rand.nextInt(26) + 65) % 91));
+    fusee.setName(StringUtils.getRandomString());
     GenericNameTest.nameStartWithMajOk(fusee.getName());
-  }
-
-  /**
-   * testNameStartWithMajOk test function.
-   */
-  @Test
-  public void testNameStartWithMajKo() {
-    Fusee fusee = new Fusee();
-
-    String c = Character.toString((rand.nextInt(126 - 32) + 32));
-
-    if (((char) c.charAt(0)) >= 65 && ((char) c.charAt(0)) <= 90) {
-      c = Character.toString(c.charAt(0) + 26);
-    }
-
-    fusee.setName(c);
-    GenericNameTest.nameStartWithMajKo(fusee.getName());
   }
 }
