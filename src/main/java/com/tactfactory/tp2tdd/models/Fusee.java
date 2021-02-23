0f666d6147478348bc5381aed2000792afb1fe3a
Antoine Cronier <antoine.cronier@tactfactory.com>
2021-02-23 11:35:10 +0100

    TU03-3

--- src/main/java/com/tactfactory/tp2tdd/models/Fusee.java
+++ src/main/java/com/tactfactory/tp2tdd/models/Fusee.java
@@ -18,6 +18,15 @@ public class Fusee {
     ) {
       throw new Exception();
     }
+    for (char item : name.substring(1).toCharArray()) {
+      if (item != '-'
+                && !(item >= 65 && item <= 90) //Majuscule
+                && !(item >= 48 && item <= 57) //Chiffre
+                && !(item >= 97 && item <= 122) //Minuscule
+          ) {
+        throw new Exception();
+      }
+    }
     this.name = Character.toString(name.charAt(0)).toUpperCase() + name.substring(1).toLowerCase();
   }
 
