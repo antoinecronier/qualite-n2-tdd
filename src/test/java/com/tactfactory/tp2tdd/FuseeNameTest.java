b6bab92c6ea2137cf01fa58229f1870ba5ba33a4
Antoine Cronier <antoine.cronier@tactfactory.com>
2021-02-23 09:42:24 +0100

    TU01-1-fix-3

--- src/test/java/com/tactfactory/tp2tdd/FuseeNameTest.java
+++ src/test/java/com/tactfactory/tp2tdd/FuseeNameTest.java
@@ -13,6 +13,26 @@ public class FuseeNameTest {
   @Test
   public void testNameStartWithMajOk() {
     Fusee fusee = new Fusee();
+    fusee.setName("AaaaA");
+    GenericNameTest.nameStartWithMajOk(fusee.getName());
+  }
+
+  /**
+   * testNameStartWithMajKo test function.
+   */
+  @Test
+  public void testNameStartWithMajKo() {
+    Fusee fusee = new Fusee();
+    fusee.setName("aAAAa");
+    GenericNameTest.nameStartWithMajOk(fusee.getName());
+  }
+
+  /**
+   * testNameStartWithMajKo test function.
+   */
+  @Test
+  public void testNameStartWithMajRandom() {
+    Fusee fusee = new Fusee();
     fusee.setName(StringUtils.getRandomString());
     GenericNameTest.nameStartWithMajOk(fusee.getName());
   }
