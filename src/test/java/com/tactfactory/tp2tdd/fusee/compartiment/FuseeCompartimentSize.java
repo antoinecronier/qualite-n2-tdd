de5170257b3cf3a6423df789a3e610cff52100b9
Antoine Cronier <antoine.cronier@tactfactory.com>
2021-02-23 12:18:53 +0100

    TU05-1-fix-1

--- src/test/java/com/tactfactory/tp2tdd/fusee/compartiment/FuseeCompartimentSize.java
+++ src/test/java/com/tactfactory/tp2tdd/fusee/compartiment/FuseeCompartimentSize.java
@@ -29,10 +29,30 @@ public class FuseeCompartimentSize {
   public void testCompartimentOk2() throws Exception {
     Fusee fusee = new Fusee();
     for (int i = 0; i < 4; i++) {
-      fusee.getCompartiements().add(new Compartiment());
+      fusee.addCompartiment(new Compartiment());
     }
 
     assertTrue(fusee.getCompartiements().size() == 6);
+  }
+
+  /**
+   * testCompartimentOk2 test function.
+   *
+   * @throws Exception to throw.
+   */
+  @Test
+  public void testCompartimentOk3() throws Exception {
+    Fusee fusee = new Fusee();
+    for (int i = 0; i < 4; i++) {
+      fusee.addCompartiment(new Compartiment());
+    }
+
+    for (int i = 0; i < 2; i++) {
+      fusee.removeCompartiment(
+              fusee.getCompartiements().get(fusee.getCompartiements().size() - 1 - i));
+    }
+
+    assertTrue(fusee.getCompartiements().size() == 4);
   }
 
   /**
@@ -52,6 +72,22 @@ public class FuseeCompartimentSize {
   }
 
   /**
+   * testCompartimentKo1b test function.
+   *
+   * @throws Exception to throw.
+   */
+  @Test
+  public void testCompartimentKo1b() throws Exception {
+    Fusee fusee = new Fusee();
+
+    assertThrows(Exception.class, () -> {
+      for (int i = 0; i < 10; i++) {
+        fusee.addCompartiment(new Compartiment());
+      }
+    });
+  }
+
+  /**
    * testCompartimentKo2 test function.
    *
    * @throws Exception to throw.
@@ -61,7 +97,7 @@ public class FuseeCompartimentSize {
     Fusee fusee = new Fusee();
 
     assertThrows(Exception.class, () -> {
-        fusee.getCompartiements().clear();
+      fusee.getCompartiements().clear();
     });
   }
 
@@ -75,7 +111,21 @@ public class FuseeCompartimentSize {
     Fusee fusee = new Fusee();
 
     assertThrows(Exception.class, () -> {
-        fusee.getCompartiements().remove(fusee.getCompartiements().get(0));
+      fusee.getCompartiements().remove(fusee.getCompartiements().get(0));
+    });
+  }
+
+  /**
+   * testCompartimentKo3b test function.
+   *
+   * @throws Exception to throw.
+   */
+  @Test
+  public void testCompartimentKo3b() throws Exception {
+    Fusee fusee = new Fusee();
+
+    assertThrows(Exception.class, () -> {
+      fusee.removeCompartiment(fusee.getCompartiements().get(0));
     });
   }
 }
