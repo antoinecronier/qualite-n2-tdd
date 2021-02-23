9845edc5140f84aa52a2e6b6eb3f88fe57038327
Antoine Cronier <antoine.cronier@tactfactory.com>
2021-02-23 10:01:07 +0100

    TU01-3

--- src/main/java/com/tactfactory/tp2tdd/models/Fusee.java
+++ src/main/java/com/tactfactory/tp2tdd/models/Fusee.java
@@ -4,8 +4,21 @@ public class Fusee {
 
   private String name;
 
-  public void setName(String name) {
-    this.name = name;
+  /**
+   * setName.
+   *
+   * @param name to return
+   * @throws Exception to throw
+   */
+  public void setName(final String name) throws Exception {
+    if (name == null || name.equals("")
+        ||
+        !((name.charAt(0) >= 65 && name.charAt(0) <= 90)
+          || (name.charAt(0) >= 97 && name.charAt(0) <= 122))
+    ) {
+      throw new Exception();
+    }
+    this.name = Character.toString(name.charAt(0)).toUpperCase() + name.substring(1);
   }
 
   public String getName() {
